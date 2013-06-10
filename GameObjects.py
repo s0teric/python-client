11c45c4e518cce81a725210a89b78479e4714c7b
Russley Shaw <russleyshaw@gmail.com>
2013-06-10 16:36:52 -0500

    Starting creation of GameObjects.py and main.py
    Added static file AI.py

--- GameObjects.py
+++ GameObjects.py
@@ -1,1 +1,17 @@
 ########## GameObjects.py ##########
+
+% for model in models
+
+#\
+#${model.doc}
+%    if model.parent:
+class ${model.name}(${model.parent.name}):
+%    else:
+class ${model.name}(GameObject):
+%    endif
+
+%    for datum in model.data:
+  def get${capitalize(datum.name)}(self):
+    return
+
+%    endfor
---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+import socket
+
+
+# DEFAULTS
+DEFAULT_IP = 'localhost'
+DEFAULT_PORT = 19000
+
+
+def main():
+  tcpIp = DEFAULT_IP
+  tcpPort = DEFAULT_PORT
+
+  if len(sys.argv) > 3 : # Too many args
+    print "More than two args supplied."
+    print "main.py [ip address] [port]"
+    print "main.py [ip address]"
+    print "main.py"
+    sys.exit(1)
+  elif len(sys.argv) == 3: # file, ip address, port
+    tcpIp = sys.argv[1]
+    tcpPort = sys.argv[2]
+  elif len(sys.argv) == 2: # file, ip address
+    tcpIp = sys.argv[1]
+
+  return
+########## RUN MAIN ##########
+
+main()
+
